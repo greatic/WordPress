13da6695183473389fee3e6ffe3fea87dadabfe4
Piotrek <almars311@gmail.com>
2017-05-11 20:49:41 +0200

    Add files via upload

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * SampleWebsite Theme Customizer
+ *
+ * @package SampleWebsite
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function samplewebsite_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'samplewebsite_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function samplewebsite_customize_preview_js() {
+	wp_enqueue_script( 'samplewebsite_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'samplewebsite_customize_preview_js' );
